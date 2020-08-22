a61ffbc2799dbd323f07771b952620187bbd62eb
Kent Pitman <kent_pitman@hms.harvard.edu>
2020-08-22 03:57:27 -0400

    WIP: Some improvements to file uploader. Still not ready for prime time.

--- submit_cgap/scripts/upload_metadata_bundle_part.py
+++ submit_cgap/scripts/upload_metadata_bundle_part.py
@@ -1,19 +1,100 @@
 import argparse
 # import datetime
-# import io
-# import json
-# import os
+import io
+import json
+import os
 # import re
 # import requests
-# import subprocess
+import subprocess
 # import sys
-# import time
+import time
 
+from dcicutils import ff_utils
+from dcicutils.misc_utils import PRINT
+from dcicutils.qa_utils import override_environ
 from dcicutils.command_utils import yes_or_no
 from .submit_metadata_bundle import resolve_site, show, get_cgap_auth_dict
 
 
 EPILOG = __doc__
+
+
+KEYPAIRS_FILENAME = '.cgap-keypairs.json'
+
+
+def get_cgap_keypairs():
+    with io.open(KEYPAIRS_FILENAME) as keyfile:
+        keys = json.load(keyfile)
+        return keys
+
+
+def get_cgap_keypair(bs_env=None):
+    """
+    Gets the appropriate auth info for talking to a given beanstalk environment.
+
+    Args:
+        bs_env: the name of a beanstalk environment
+
+    Returns:
+        Auth information.
+        The auth is probably a keypair, though we might change this to include a JWT token in the the future.
+    """
+    keypairs = get_cgap_keypairs()
+    keypair = keypairs.get(bs_env or 'cgap-local')
+    if not keypair:
+        raise RuntimeError("Missing credential in keypairs file %s for %s." % (KEYPAIRS_FILENAME, bs_env))
+
+
+def execute_prearranged_upload(path, *, upload_credentials):
+    """
+    This performs a file upload using special credentials received from ff_utils.patch_metadata.
+
+    :param path: the name of a local file to upload
+    :param upload_credentials: a dictionary of credentials to be used for the upload,
+        containing the keys 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', and 'AWS_SECURITY_TOKEN'.
+    """
+
+    try:
+        env = dict(os.environ,
+                   AWS_ACCESS_KEY_ID=upload_credentials['AccessKeyId'],
+                   AWS_SECRET_ACCESS_KEY=upload_credentials['SecretAccessKey'],
+                   AWS_SECURITY_TOKEN=upload_credentials['SessionToken'])
+    except Exception as e:
+        raise("Upload specification is not in good form. %s: %s" % (e.__class__.__name__, e))
+
+    PRINT("Uploading file.")
+    start = time.time()
+    try:
+        subprocess.check_call(['aws', 's3', 'cp', '--only-show-errors', path, upload_credentials['upload_url']], env=env)
+    except subprocess.CalledProcessError as e:
+        PRINT("Upload failed with exit code %d" % e.returncode)
+    else:
+        end = time.time()
+        duration = end - start
+        PRINT("Uploaded in %.2f seconds" % duration)
+
+
+def upload_file_to_uuid(filename, *, uuid, auth):
+    """
+    Upload file to a target environment.
+
+    :param upload_specification: a dictionary containing a 'filename' and 'uuid',
+        identifying the file to upload and the item into which it is to be uploaded.
+    :param auth: auth info in the form of a dictionary containing 'key', 'secret', and 'server'.
+    """
+
+    # filename here should not include path
+    patch_data = {'filename': os.path.basename(filename)}
+
+    response = ff_utils.patch_metadata(patch_data, uuid, key=auth)
+
+    try:
+        [metadata] = response['@graph']
+        upload_credentials = metadata['upload_credentials']
+    except Exception:
+        raise RuntimeError("Unable to obtain upload credentials for file %s." % filename)
+
+    execute_prearranged_upload(filename, upload_credentials=upload_credentials)
 
 
 def main(simulated_args_for_testing=None):
@@ -29,16 +110,20 @@ def main(simulated_args_for_testing=None):
     args = parser.parse_args(args=simulated_args_for_testing)
 
     part_filename = args.part_filename
+    uuid = args.uuid
 
     site = resolve_site(site=args.site, env=args.env)
 
     auth_dict = get_cgap_auth_dict(site)
 
+    # print("auth_dict=", json.dumps(auth_dict, indent=2))
+
     if not yes_or_no("Upload %s to %s?" % (part_filename, site)):
         show("Aborting submission.")
         exit(1)
 
-    raise NotImplementedError("This is not yet implemented.")
+    upload_file_to_uuid(filename=part_filename, uuid=uuid, auth=auth_dict)
+
 
 # TODO: Borrow from the following? -kmp 18-Aug-2020
 #
