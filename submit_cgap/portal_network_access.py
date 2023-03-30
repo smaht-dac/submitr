b30bd7e8186609666e652e899e0c42bea4b40f04
David Michaels <david_michaels@hms.harvard.edu>
2023-03-30 17:10:39 -0400

    Changes based on PR-37 feedback.

--- submit_cgap/portal_network_access.py
+++ submit_cgap/portal_network_access.py
@@ -1,72 +1,26 @@
 # This file contains centralized functions for all Portal interactions used by SubmitCGAP.
 
-import io
-import json
 import requests
-from typing import Callable, Optional, Tuple, Union
+from typing import Tuple
 from dcicutils import ff_utils
-from dcicutils.misc_utils import environ_bool, PRINT
+from .trace_utils import Trace
 
 
-DEBUG_PROTOCOL = environ_bool("DEBUG_PROTOCOL", default=False)
-
-
+@Trace()
 def portal_metadata_post(schema: str, data: dict, auth: Tuple) -> dict:
-
-    if DEBUG_PROTOCOL:  # pragma: no cover
-        PRINT(f"DEBUG: METADATA POST {schema} | DATA: {json.dumps(data)}")
-
-    response = ff_utils.post_metadata(post_item=data, schema_name=schema, key=auth)
-
-    if DEBUG_PROTOCOL:  # pragma: no cover
-        PRINT(f"DEBUG: METADATA POST {schema} -> {response.get('status')}"
-              f" | RESPONSE: {json.dumps(response, default=str)}")
-
-    return response
+    return ff_utils.post_metadata(post_item=data, schema_name=schema, key=auth)
 
 
+@Trace()
 def portal_metadata_patch(uuid: str, data: dict, auth: Tuple) -> dict:
-
-    if DEBUG_PROTOCOL:  # pragma: no cover
-        PRINT(f"DEBUG: METADATA PATCH {uuid} | DATA: {json.dumps(data)}")
-
-    response = ff_utils.patch_metadata(patch_item=data, obj_id=uuid, key=auth)
-
-    if DEBUG_PROTOCOL:  # pragma: no cover
-        PRINT(f"DEBUG: METADATA PATCH {uuid} -> {response.get('status')}"
-              f" | RESPONSE: {json.dumps(response, default=str)}")
-
-    return response
+    return ff_utils.patch_metadata(patch_item=data, obj_id=uuid, key=auth)
 
 
+@Trace()
 def portal_request_get(url: str, auth: Tuple, **kwargs) -> requests.models.Response:
-    return _portal_request(requests.get, url=url, auth=auth, **kwargs)
+    return requests.get(url, auth=auth, **kwargs)
 
 
+@Trace()
 def portal_request_post(url: str, auth: Tuple, **kwargs) -> requests.models.Response:
-    return _portal_request(requests.post, url=url, auth=auth, **kwargs)
-
-
-def _portal_request(request: Callable, url: str, auth: Tuple, **kwargs) -> requests.models.Response:
-
-    if DEBUG_PROTOCOL:  # pragma: no cover
-        PRINT(f"DEBUG: HTTP {request.__name__.upper()} {url}", end="")
-        if auth:
-            PRINT(f" | AUTH: <REDACTED>", end="")
-        if kwargs.get("headers"):
-            PRINT(f" | HEADERS: {json.dumps(kwargs['headers'], default=str)}", end="")
-        if kwargs.get("data"):
-            PRINT(f" | DATA: {json.dumps(kwargs['data'], default=str)}", end="")
-        if kwargs.get("json"):
-            PRINT(f" | JSON: {json.dumps(kwargs['json'], default=str)}", end="")
-        if kwargs.get("files"):
-            PRINT(f" | FILES: {json.dumps(kwargs['files'], default=str)}", end="")
-        PRINT()
-
-    response = request(url, auth=auth, allow_redirects=True, **kwargs)
-
-    if DEBUG_PROTOCOL:  # pragma: no cover
-        PRINT(f"DEBUG: HTTP {request.__name__.upper()} {url} -> {response.status_code}"
-              f" | RESPONSE: {json.dumps(response.json(), default=str)}")
-
-    return response
+    return requests.post(url, auth=auth, **kwargs)
---
+++ submit_cgap/trace_utils.py
@@ -0,0 +1,38 @@
+from dcicutils.misc_utils import environ_bool, full_object_name, PRINT
+from dcicutils.obfuscation_utils import obfuscate
+import functools
+
+
+DEBUG_PROTOCOL = environ_bool("DEBUG_PROTOCOL", default=False)
+
+
+def Trace(enabled=None):
+    if enabled is None:
+        enabled = DEBUG_PROTOCOL
+    def _maybe_attach_trace(fn):
+        if not enabled:
+            return fn
+        trace_name = full_object_name(fn)
+        @functools.wraps(fn)
+        def _traced(*args, **kwargs):
+            PRINT(f"TRACE {trace_name} | ARGS: {args!r} KWARGS: {_show_dict(kwargs)}")
+            try:
+                res = fn(*args, **kwargs)
+                PRINT(f"TRACE {trace_name} RETURN: {res!r}")
+                return res
+            except BaseException as exc:
+                PRINT(f"TRACE {trace_name} EXCEPTION: {get_error_message(exc)}")
+                raise
+        return _traced
+    return _maybe_attach_trace
+
+
+def _show_dict(d, indent=0):
+    d = obfuscate(d)
+    if not d:
+        return "{}"
+    else:
+        s = "dict("
+        for k, v in d.items():
+            s += f"{' ' * indent}{k}={v!r}"
+        return s + " " * indent + ")"
