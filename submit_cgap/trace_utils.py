3ed9d0e53ef01805346dca7c82ebb93bd5ff5b23
David Michaels <david_michaels@hms.harvard.edu>
2023-03-30 17:34:23 -0400

    Changes based on PR-37 feedback.

--- submit_cgap/trace_utils.py
+++ submit_cgap/trace_utils.py
@@ -2,6 +2,7 @@ from dcicutils.misc_utils import environ_bool, full_object_name, PRINT
 from dcicutils.obfuscation_utils import obfuscate_dict
 import functools
 import json
+import requests
 
 
 DEBUG_PROTOCOL = environ_bool("DEBUG_PROTOCOL", default=False)
@@ -16,10 +17,19 @@ def Trace(enabled=None):
         trace_name = full_object_name(fn)
         @functools.wraps(fn)
         def _traced(*args, **kwargs):
-            PRINT(f"TRACE {trace_name} | ARGS: {args!r} KWARGS: {_show_dict(kwargs)}")
+            PRINT(f"TRACE {trace_name} | ARGS: {args!r} KWARGS: {_dict_to_str(kwargs)}")
             try:
                 res = fn(*args, **kwargs)
-                PRINT(f"TRACE {trace_name} RETURN: {res!r}")
+                if isinstance(res, dict):
+                    PRINT(f"TRACE {trace_name} RETURN: {_dict_to_str(res)}")
+                elif isinstance(res, requests.models.Response):
+                    try:
+                        res_dict = res.json()
+                        PRINT(f"TRACE {trace_name} RETURN: {_dict_to_str(res_dict)}")
+                    except Exception:
+                        PRINT(f"TRACE {trace_name} RETURN: {res!r}")
+                else:
+                    PRINT(f"TRACE {trace_name} RETURN: {res!r}")
                 return res
             except BaseException as exc:
                 PRINT(f"TRACE {trace_name} EXCEPTION: {get_error_message(exc)}")
@@ -28,7 +38,8 @@ def Trace(enabled=None):
     return _maybe_attach_trace
 
 
-def _show_dict(d, indent=0):
+def _dict_to_str(d, indent=0):
+    # TODO: It appears the obfuscate_dict does not handle dictionaries within arrays.
     d = obfuscate_dict(d)
     if not isinstance(d, dict):
         return ""
