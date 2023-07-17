58e730fbeb4b4049c81bd32a227c0eaf756a5031
Kent Pitman <kent_pitman@hms.harvard.edu>
2023-07-17 16:02:30 -0400

    Better defaulting of the --app argument for check-submission.

--- submit_cgap/scripts/check_submission.py
+++ submit_cgap/scripts/check_submission.py
@@ -1,5 +1,6 @@
 import argparse
-from dcicutils.common import APP_FOURFRONT, ORCHESTRATED_APPS
+from dcicutils.common import APP_CGAP, ORCHESTRATED_APPS
+from dcicutils.env_utils import EnvUtils
 from ..submission import check_submit_ingestion
 from ..utils import script_catch_errors
 
@@ -7,19 +8,26 @@ from ..utils import script_catch_errors
 EPILOG = __doc__
 
 
-def main():
+def default_app_for_check_submission():
+    EnvUtils.init()
+    return EnvUtils.app_name()
+
+
+def main(simulated_args_for_testing=None):
+    default_app = default_app_for_check_submission()
+
     parser = argparse.ArgumentParser(  # noqa - PyCharm wrongly thinks the formatter_class is invalid
         description="Check previously submitted submission.",
         epilog=EPILOG,
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
     parser.add_argument('submission_uuid', help='uuid of previously submitted submission.')
-    parser.add_argument('--app', choices=ORCHESTRATED_APPS, default=APP_FOURFRONT,
-                        help=f"An application (default {APP_FOURFRONT!r}. Only for debugging."
+    parser.add_argument('--app', choices=ORCHESTRATED_APPS, default=default_app,
+                        help=f"An application (default {default_app!r}. Only for debugging."
                              f" Normally this should not be given.")
     parser.add_argument('--server', '-s', help="an http or https address of the server to use", default=None)
     parser.add_argument('--env', '-e', help="a CGAP beanstalk environment name for the server to use", default=None)
-    args = parser.parse_args()
+    args = parser.parse_args(args=simulated_args_for_testing)
 
     with script_catch_errors():
         return check_submit_ingestion(
