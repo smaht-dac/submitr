f358172373a6cb6bb78b24734415c87a2f2ad200
David Michaels <david_michaels@hms.harvard.edu>
2024-02-05 12:18:03 -0500

    trying to get .readthedocs.yml working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,8 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx-rtd-theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,8 +46,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
