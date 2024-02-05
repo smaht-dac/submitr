cac02a3c2e841efcf6e7dc91254b2bf9435268b8
David Michaels <david_michaels@hms.harvard.edu>
2024-02-05 12:33:44 -0500

    trying to get .readthedocs.yml working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,8 +28,7 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx-rtd-theme'
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
