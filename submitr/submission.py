0d264a7cc687ab7aabd70a144b242c11e448131b
David Michaels <david_michaels@hms.harvard.edu>
2024-03-25 14:01:03 -0400

    bug printing errors

--- submitr/submission.py
+++ submitr/submission.py
@@ -1404,8 +1404,11 @@ def _print_submission_summary(portal: Portal, result: dict,
     if additional_data := result.get("additional_data"):
         if (validation_info := additional_data.get("validation_output")) and isinstance(validation_info, dict):
             # TODO: Cleanup/unify error structure from client and server!
-            if ref_errors := _validate_references(validation_info["ref"], None):
+            if ref_errors := _validate_references(validation_info.get("ref"), None):
                 errors.extend(_format_reference_errors(ref_errors, verbose=verbose, debug=debug))
+            if validation_errors := validation_info.get("validation"):
+                for validation_error in validation_errors:
+                    errors.append(f"- {_format_issue(validation_error)}")
     if processing_status := result.get("processing_status"):
         summary_lines = []
         if additional_data := result.get("additional_data"):
