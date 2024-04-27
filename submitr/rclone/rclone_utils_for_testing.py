b1afc85db396e3bedf4101d7034755d4b7217c26
David Michaels <david_michaels@hms.harvard.edu>
2024-04-26 23:06:09 -0400

    Minor updates to scripts.local_check_execution to sanity check ElasticSearch connection.

--- submitr/rclone/rclone_utils_for_testing.py
+++ submitr/rclone/rclone_utils_for_testing.py
@@ -5,16 +5,13 @@ import configparser
 from datetime import timedelta
 import os
 from typing import List, Optional, Union
-from dcicutils.file_utils import are_files_equal, create_random_file, normalize_file_path
-from dcicutils.misc_utils import create_dict
+from dcicutils.file_utils import are_files_equal
 from dcicutils.tmpfile_utils import temporary_file
 from dcicutils.datetime_utils import format_datetime
-from submitr.rclone.rclone_config import RCloneConfig
 from submitr.rclone.rclone_config_amazon import AmazonCredentials
 
 
-# Module with class/functions to aid in
-# integration testing of smaht-submitr rclone support.
+# Module with class/functions to aid in integration testing of smaht-submitr rclone support.
 
 class AwsCredentials(AmazonCredentials):
 
@@ -31,37 +28,19 @@ class AwsCredentials(AmazonCredentials):
                  session_token: Optional[str] = None,
                  kms_key_id: Optional[str] = None) -> None:
 
+        if isinstance(credentials, str):
+            credentials = AwsCredentials.get_credentials_from_file(credentials, credentials_section)
         if isinstance(credentials, AmazonCredentials):
             super().__init__(credentials)
-            self._credentials_file = None
-        elif (isinstance(credentials, str) and
-              (credentials := AwsCredentials.get_credentials_from_file(credentials, credentials_section))):
-            super().__init__(region=credentials.get("region"),
-                             access_key_id=credentials.get("access_key_id"),
-                             secret_access_key=credentials.get("secret_access_key"),
-                             session_token=credentials.get("session_token"))
-            self._credentials_file = credentials.get("credentials_file")
         else:
-            super().__init__()
-            self._credentials_file = None
-
-        if region := RCloneConfig._normalize_string_value(region):
-            self.region = region
-        if access_key_id := RCloneConfig._normalize_string_value(access_key_id):
-            self.access_key_id = access_key_id
-        if secret_access_key := RCloneConfig._normalize_string_value(secret_access_key):
-            self.secret_access_key = secret_access_key
-        if session_token := RCloneConfig._normalize_string_value(session_token):
-            self.session_token = session_token
-        if kms_key_id := RCloneConfig._normalize_string_value(kms_key_id):
-            self.kms_key_id = kms_key_id
-
-    @property
-    def credentials_file(self) -> Optional[str]:
-        return self._credentials_file
+            super().__init__(region=region,
+                             access_key_id=access_key_id,
+                             secret_access_key=secret_access_key,
+                             session_token=session_token,
+                             kms_key_id=kms_key_id)
 
     @staticmethod
-    def get_credentials_from_file(credentials_file: str, credentials_section: str = None) -> Optional[dict]:
+    def get_credentials_from_file(credentials_file: str, credentials_section: str = None) -> AmazonCredentials:
         if not credentials_section:
             credentials_section = "default"
         try:
@@ -73,35 +52,34 @@ class AwsCredentials(AmazonCredentials):
                     credentials_file = os.path.join(f"~/.aws_test.{credentials_file}/credentials")
             config = configparser.ConfigParser()
             config.read(os.path.expanduser(credentials_file))
-            credentials = config[credentials_section]
-            region = (credentials.get("region", None) or
-                      credentials.get("region_name", None) or
-                      credentials.get("aws_default_region", None))
-            access_key_id = (credentials.get("aws_access_key_id", None) or
-                             credentials.get("access_key_id", None))
-            secret_access_key = (credentials.get("aws_secret_access_key", None) or
-                                 credentials.get("secret_access_key", None))
-            session_token = (credentials.get("aws_session_token", None) or
-                             credentials.get("session_token", None))
-            return create_dict(
-                region=region,
-                access_key_id=access_key_id,
-                secret_access_key=secret_access_key,
-                session_token=session_token,
-                credentials_file=normalize_file_path(credentials_file))
+            section = config[credentials_section]
+            region = (section.get("region", None) or
+                      section.get("region_name", None) or
+                      section.get("aws_default_region", None))
+            access_key_id = (section.get("aws_access_key_id", None) or
+                             section.get("access_key_id", None))
+            secret_access_key = (section.get("aws_secret_access_key", None) or
+                                 section.get("secret_access_key", None))
+            session_token = (section.get("aws_session_token", None) or
+                             section.get("session_token", None))
+            return AmazonCredentials(region=region,
+                                     access_key_id=access_key_id,
+                                     secret_access_key=secret_access_key,
+                                     session_token=session_token)
         except Exception:
-            return None
+            pass
+        return AmazonCredentials()
 
     @staticmethod
-    def get_credentials_from_environment_variables() -> dict:
-        return create_dict(
+    def get_credentials_from_environment_variables() -> AmazonCredentials:
+        return AmazonCredentials(
             region=os.environ.get("AWS_DEFAULT_REGION", None),
             access_key_id=os.environ.get("AWS_ACCESS_KEY_ID", None),
             secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY", None),
             session_token=os.environ.get("AWS_SESSION_TOKEN", None))
 
     @staticmethod
-    def clear_credentials_from_environment_variables() -> None:
+    def remove_credentials_from_environment_variables() -> None:
         os.environ.pop("AWS_DEFAULT_REGION", None)
         os.environ.pop("AWS_ACCESS_KEY_ID", None)
         os.environ.pop("AWS_SECRET_ACCESS_KEY", None)
@@ -117,9 +95,8 @@ class AwsS3:
     def create(*args, **kwargs) -> AwsS3:
         return AwsS3(*args, **kwargs)
 
-    def __init__(self, credentials: AmazonCredentials, default_bucket: Optional[str] = None) -> None:
+    def __init__(self, credentials: AmazonCredentials) -> None:
         self._credentials = AwsCredentials(credentials)
-        self._default_bucket = default_bucket
         self._client = boto3.client(
             "s3",
             region_name=self._credentials.region,
@@ -131,34 +108,19 @@ class AwsS3:
     def credentials(self) -> AwsCredentials:
         return self._credentials
 
-    @credentials.setter
-    def credentials(self, value: AwsCredentials) -> None:
-        if isinstance(value, AwsCredentials):
-            self._credentials = value
-
     @property
     def client(self) -> BotoClient:
         return self._client
-
-    @property
-    def default_bucket(self) -> Optional[str]:
-        return self._default_bucket
-
-    @default_bucket.setter
-    def default_bucket(self, value: str) -> Optional[str]:
-        self._default_bucket = value
 
     def upload_file(self, file: str, bucket: str, key: Optional[str] = None, raise_exception: bool = True) -> bool:
         try:
             if not key:
                 key = os.path.basename(file)
-            import pdb ; pdb.set_trace()
-            pass
             if kms_key_id := self.credentials.kms_key_id:
                 # Note that it is not necessary to use the KMS Key ID when downloading
                 # a file uploaded with a KMS Key ID, since this info is stored together
                 # with the uploaded file and is used if applicable.
-                extra_args = {"ServerSideEncryption": "aws:kms", "SSEKMSKeyId": self.credentials.kms_key_id}
+                extra_args = {"ServerSideEncryption": "aws:kms", "SSEKMSKeyId": kms_key_id}
             else:
                 extra_args = None
             self.client.upload_file(file, bucket, key, ExtraArgs=extra_args)
@@ -170,6 +132,8 @@ class AwsS3:
 
     def download_file(self, bucket: str, key: str, file: str, raise_exception: bool = True) -> bool:
         try:
+            if os.path.isdir(file):
+                file = f"{file}/{key.replace(os.sep, '_')}"
             self.client.download_file(bucket, key, file)
             return True
         except Exception as e:
@@ -185,6 +149,29 @@ class AwsS3:
                 self.client.delete_object(Bucket=bucket, Key=key)
                 return True
         except Exception as e:
+            if raise_exception is True:
+                raise e
+        return False
+
+    def file_exists(self, bucket: str, key: str, raise_exception: bool = True) -> bool:
+        try:
+            self.client.head_object(Bucket=bucket, Key=key)
+            return True
+        except Exception as e:
+            if hasattr(e, "response") and e.response.get("Error", {}).get("Code") == "404":
+                return False
+            if raise_exception is True:
+                raise e
+        return False
+
+    def file_equals(self, bucket: str, key: str, file: str, raise_exception: bool = True) -> bool:
+        try:
+            with temporary_file() as temporary_downloaded_file_name:
+                if self.download_file(bucket, key, temporary_downloaded_file_name):
+                    return are_files_equal(file, temporary_downloaded_file_name)
+        except Exception as e:
+            if hasattr(e, "response") and e.response.get("Error", {}).get("Code") == "404":
+                return False
             if raise_exception is True:
                 raise e
         return False
@@ -223,73 +210,34 @@ class AwsS3:
             keys = keys[:count]
         return keys
 
-    def file_exists(self, bucket: str, key: str, raise_exception: bool = True) -> bool:
-        try:
-            self.client.head_object(Bucket=bucket, Key=key)
-            return True
-        except Exception as e:
-            if hasattr(e, "response") and e.response.get("Error", {}).get("Code") == "404":
-                return False
-            if raise_exception is True:
-                raise e
-        return False
-
-    def file_equals(self, bucket: str, key: str, file: str, raise_exception: bool = True) -> bool:
-        try:
-            with temporary_file() as temporary_downloaded_file_name:
-                if self.download_file(bucket, key, temporary_downloaded_file_name):
-                    return self.are_files_equal(file, temporary_downloaded_file_name)
-        except Exception as e:
-            if hasattr(e, "response") and e.response.get("Error", {}).get("Code") == "404":
-                return False
-            if raise_exception is True:
-                raise e
-        return False
-
-    @staticmethod
-    def are_files_equal(filea: str, fileb: str) -> bool:
-        return are_files_equal(filea, fileb)
-
-    @staticmethod
-    def create_random_file(file: Optional[str] = None,
-                           prefix: Optional[str] = None, suffix: Optional[str] = None,
-                           nbytes: int = 1024, binary: bool = False) -> str:
-        return create_random_file(file=file, prefix=prefix, suffix=suffix, nbytes=nbytes, binary=binary)
-
     def generate_temporary_credentials(self,
                                        duration: Optional[Union[int, timedelta]] = None,
                                        bucket: Optional[str] = None, key: Optional[str] = None,
                                        readonly: bool = False) -> AwsCredentials:
         """
-        Generates and returns temporary AWS credentials. The default duration of validity
-        for the generated credential is one hour; this can be overridden by specifying
-        the duration argument (which is in seconds). By default the generated credentials
-        will have full S3 access; but if the readonly argument is True then this will be
-        limited to readonly S3 access. Passing in a bucket or a bucket and key argument(s)
-        will further limit access to just the specified bucket or bucket and key.
+        Generates and returns temporary AWS credentials. The default duration of validity for
+        the generated credential is one hour; this can be overridden by specifying the duration
+        argument (which is in seconds). By default the generated credentials will have full S3
+        access (AmazonS3FullAccess); but if the readonly argument is True then this will be
+        limited to readonly S3 access (AmazonS3ReadOnlyAccess). Passing bucket or bucket/key
+        key argument/s will further limit access to just the specified bucket or bucket/key.
         """
-        policy_resources = ["*"]
-        policy_include_deny = False
+        resources = ["*"]
+        include_deny = False
         if isinstance(bucket, str) and bucket:
             if isinstance(key, str) and bucket:
-                policy_resources = ["arn:aws:s3:::{bucket}/{key}"]
+                resources = ["arn:aws:s3:::{bucket}/{key}"]
             else:
-                policy_resources = ["arn:aws:s3:::{bucket}", "arn:aws:s3:::{bucket}/*"]
-                policy_include_deny = True
-
+                resources = ["arn:aws:s3:::{bucket}", "arn:aws:s3:::{bucket}/*"]
+                include_deny = True
         if readonly:
-            # AmazonS3ReadOnlyAccess
-            policy_actions = ["s3:Get*", "s3:Head*", "s3:List*", "s3:Describe*",
-                              "s3-object-lambda:Get*", "s3-object-lambda:Head*", "s3-object-lambda:List*"]
+            actions = ["s3:Get*", "s3:Head*", "s3:List*", "s3:Describe*",
+                       "s3-object-lambda:Get*", "s3-object-lambda:Head*", "s3-object-lambda:List*"]
         else:
-            # AmazonS3FullAccess
-            policy_actions = ["s3:*", "s3-object-lambda:*"]
-
+            actions = ["s3:*", "s3-object-lambda:*"]
         statements = []
-        statements.append({"Effect": "Allow", "Action": policy_actions, "Resource": policy_resources})
-        if policy_include_deny:
-            statements.append = [{"Effect": "Deny", "Action": policy_actions, "NotResource": policy_resources}]
-
+        statements.append({"Effect": "Allow", "Action": actions, "Resource": resources})
+        if include_deny:
+            statements.append = [{"Effect": "Deny", "Action": actions, "NotResource": resources}]
         policy = {"Version": "2012-10-17", "Statement": statements}
-
         return self.credentials.generate_temporary_credentials(duration=duration, policy=policy)
