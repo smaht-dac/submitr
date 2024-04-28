3b8031948c6f4f1cd851d20f01a055bd59f95ba8
David Michaels <david_michaels@hms.harvard.edu>
2024-04-28 11:27:17 -0400

    rclone updates

--- submitr/rclone/testing/rclone_utils_google_for_testing.py
+++ submitr/rclone/testing/rclone_utils_google_for_testing.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import os
 from google.cloud.storage import Client as GcsClient
 from typing import List, Optional
 from submitr.rclone.rclone_config_google import GoogleCredentials
@@ -28,10 +29,22 @@ class Gcs:
         return self._client
 
     def upload_file(self, file: str, bucket: str, key: Optional[str] = None, raise_exception: bool = True) -> bool:
-        pass
+        try:
+            if not key:
+                key = os.path.basename(file)
+            bucket = self.client.get_bucket(bucket)
+            blob = bucket.blob(key)
+            blob.upload_from_filename(file)
+            return True
+        except Exception as e:
+            if raise_exception is True:
+                raise e
+            return False
 
     def download_file(self, bucket: str, key: str, file: str, raise_exception: bool = True) -> bool:
         try:
+            if os.path.isdir(file):
+                file = f"{file}/{key.replace(os.sep, '_')}"
             bucket = self.client.get_bucket(bucket)
             blob = bucket.blob(key)
             blob.download_to_filename(file)
