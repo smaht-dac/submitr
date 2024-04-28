d888b477f78f86ecf711667d83324e76c0cc5f8f
David Michaels <david_michaels@hms.harvard.edu>
2024-04-28 15:16:57 -0400

    rclone updates

--- submitr/rclone/testing/rclone_integration_tests.py
+++ submitr/rclone/testing/rclone_integration_tests.py
@@ -16,8 +16,18 @@ from submitr.rclone.testing.rclone_utils_google_for_testing import Gcs
 # Need valid AWS credentials for (currently) smaht-wolf.
 # Wonder how to possibly test this as unit test within GA?
 
+TEMPORARY_TEST_FILE_PREFIX = "test-submitr-rclone-"
+TEMPORARY_TEST_FILE_SUFFIX = ".txt"
 
-class AmazonSmahtWolf:
+
+@contextmanager
+def temporary_test_file():
+    with temporary_random_file(prefix=TEMPORARY_TEST_FILE_PREFIX,
+                               suffix=TEMPORARY_TEST_FILE_SUFFIX) as tmp_file_path:
+        yield tmp_file_path, os.path.basename(tmp_file_path)
+
+
+class AmazonTestEnv:
 
     # Specifying the env name here (as smaht-wolf) will cause
     # AwsCredentials to read from: ~/.aws_test.smaht-wolf/credentials
@@ -27,19 +37,10 @@ class AmazonSmahtWolf:
     kms_key_id = "27d040a3-ead1-4f5a-94ce-0fa6e7f84a95"
 
     bucket = "smaht-unit-testing-files"
-    temporary_test_file_prefix = "test-submitr-rclone-"
-    temporary_test_file_suffix = ".txt"
 
     @staticmethod
     def credentials():
-        return AwsCredentials.get_credentials_from_file(AmazonSmahtWolf.env, kms_key_id=AmazonSmahtWolf.kms_key_id)
-
-    @staticmethod
-    @contextmanager
-    def temporary_test_file():
-        with temporary_random_file(prefix=AWS_ENV.temporary_test_file_prefix,
-                                   suffix=AWS_ENV.temporary_test_file_suffix) as tmp_file_path:
-            yield tmp_file_path, os.path.basename(tmp_file_path)
+        return AwsCredentials.get_credentials_from_file(AmazonTestEnv.env, kms_key_id=AmazonTestEnv.kms_key_id)
 
 
 class GoogleTestEnv:
@@ -48,29 +49,16 @@ class GoogleTestEnv:
     service_account_file = "/Users/dmichaels/.config/google-cloud/smaht-dac-617e0480d8e2.json"
 
     bucket = "smaht-submitr-rclone-testing"
-    temporary_test_file_prefix = "test-submitr-rclone-"
-    temporary_test_file_suffix = ".txt"
 
     @staticmethod
     def credentials():
         return GoogleCredentials(location=GoogleTestEnv.location,
                                  service_account_file=GoogleTestEnv.service_account_file)
 
-    @staticmethod
-    @contextmanager
-    def temporary_test_file():
-        with temporary_random_file(prefix=GCS_ENV.temporary_test_file_prefix,
-                                   suffix=GCS_ENV.temporary_test_file_suffix) as tmp_file_path:
-            yield tmp_file_path, os.path.basename(tmp_file_path)
-
-
-AWS_ENV = AmazonSmahtWolf
-GCS_ENV = GoogleTestEnv
-
 
 def test_utils_for_testing():
 
-    credentials = AWS_ENV.credentials()
+    credentials = AmazonTestEnv.credentials()
     _test_utils_for_testing(credentials)
 
     temporary_credentials = AwsS3(credentials).generate_temporary_credentials()
@@ -87,41 +75,41 @@ def _test_utils_for_testing(credentials):
     s3 = AwsS3(credentials)
     assert s3.credentials == credentials
 
-    with AWS_ENV.temporary_test_file() as (tmp_test_file_path, tmp_test_file_name):
-        assert s3.upload_file(tmp_test_file_path, AWS_ENV.bucket) is True
-        assert s3.file_exists(AWS_ENV.bucket, tmp_test_file_name) is True
-        assert s3.file_equals(AWS_ENV.bucket, tmp_test_file_name, tmp_test_file_path) is True
-        assert s3.file_exists(AWS_ENV.bucket, tmp_test_file_name + "-junk-suffix") is False
-        assert len(s3_test_files := s3.list_files(AWS_ENV.bucket, prefix=AWS_ENV.temporary_test_file_prefix)) > 0
+    with temporary_test_file() as (tmp_test_file_path, tmp_test_file_name):
+        assert s3.upload_file(tmp_test_file_path, AmazonTestEnv.bucket) is True
+        assert s3.file_exists(AmazonTestEnv.bucket, tmp_test_file_name) is True
+        assert s3.file_equals(AmazonTestEnv.bucket, tmp_test_file_name, tmp_test_file_path) is True
+        assert s3.file_exists(AmazonTestEnv.bucket, tmp_test_file_name + "-junk-suffix") is False
+        assert len(s3_test_files := s3.list_files(AmazonTestEnv.bucket, prefix=TEMPORARY_TEST_FILE_PREFIX)) > 0
         assert len(s3_test_files_found := [f for f in s3_test_files if f["key"] == tmp_test_file_name]) == 1
         assert s3_test_files_found[0]["key"] == tmp_test_file_name
-        assert len(s3.list_files(AWS_ENV.bucket, prefix=tmp_test_file_name)) == 1
+        assert len(s3.list_files(AmazonTestEnv.bucket, prefix=tmp_test_file_name)) == 1
         with temporary_random_file() as some_random_file_path:
-            assert s3.file_equals(AWS_ENV.bucket, tmp_test_file_name, some_random_file_path) is False
+            assert s3.file_equals(AmazonTestEnv.bucket, tmp_test_file_name, some_random_file_path) is False
         with temporary_file() as tmp_downloaded_file_path:
-            assert s3.download_file(AWS_ENV.bucket, tmp_test_file_name, tmp_downloaded_file_path) is True
-            assert s3.download_file(AWS_ENV.bucket, tmp_test_file_name, "/dev/null") is True
+            assert s3.download_file(AmazonTestEnv.bucket, tmp_test_file_name, tmp_downloaded_file_path) is True
+            assert s3.download_file(AmazonTestEnv.bucket, tmp_test_file_name, "/dev/null") is True
             assert are_files_equal(tmp_test_file_path, tmp_downloaded_file_path) is True
             assert are_files_equal(tmp_test_file_path, "/dev/null") is False
         with temporary_directory() as tmp_download_directory:
-            assert s3.download_file(AWS_ENV.bucket, tmp_test_file_name, tmp_download_directory) is True
+            assert s3.download_file(AmazonTestEnv.bucket, tmp_test_file_name, tmp_download_directory) is True
             assert are_files_equal(tmp_test_file_path, f"{tmp_download_directory}/{tmp_test_file_name}") is True
-        assert s3.delete_file(AWS_ENV.bucket, tmp_test_file_name) is True
-        assert s3.file_exists(AWS_ENV.bucket, tmp_test_file_name) is False
-        assert s3.file_equals(AWS_ENV.bucket, tmp_test_file_name, "/dev/null") is False
-        assert s3.download_file(AWS_ENV.bucket, tmp_test_file_name, "/dev/null") is False
+        assert s3.delete_file(AmazonTestEnv.bucket, tmp_test_file_name) is True
+        assert s3.file_exists(AmazonTestEnv.bucket, tmp_test_file_name) is False
+        assert s3.file_equals(AmazonTestEnv.bucket, tmp_test_file_name, "/dev/null") is False
+        assert s3.download_file(AmazonTestEnv.bucket, tmp_test_file_name, "/dev/null") is False
 
 
 def test_local_to_amazon():
 
-    _test_local_to_amazon(AWS_ENV.credentials())
-    _test_local_to_amazon(AWS_ENV.credentials(), nokms=True)
+    _test_local_to_amazon(AmazonTestEnv.credentials())
+    _test_local_to_amazon(AmazonTestEnv.credentials(), nokms=True)
 
-    _test_local_to_amazon(AWS_ENV.credentials(), use_temporary_credentials=True)
-    _test_local_to_amazon(AWS_ENV.credentials(), use_temporary_credentials=True, nokms=True)
+    _test_local_to_amazon(AmazonTestEnv.credentials(), use_temporary_credentials=True)
+    _test_local_to_amazon(AmazonTestEnv.credentials(), use_temporary_credentials=True, nokms=True)
 
-    _test_local_to_amazon(AWS_ENV.credentials(), use_temporary_credentials_key_specific=True)
-    _test_local_to_amazon(AWS_ENV.credentials(), use_temporary_credentials_key_specific=True, nokms=True)
+    _test_local_to_amazon(AmazonTestEnv.credentials(), use_temporary_credentials_key_specific=True)
+    _test_local_to_amazon(AmazonTestEnv.credentials(), use_temporary_credentials_key_specific=True, nokms=True)
 
 
 def _test_local_to_amazon(credentials: AmazonCredentials,
@@ -132,9 +120,9 @@ def _test_local_to_amazon(credentials: AmazonCredentials,
     if nokms is True:
         credentials = AmazonCredentials(credentials)
         credentials.kms_key_id = None
-    with AWS_ENV.temporary_test_file() as (tmp_test_file_path, tmp_test_file_name):
+    with temporary_test_file() as (tmp_test_file_path, tmp_test_file_name):
         if use_temporary_credentials_key_specific is True:
-            credentials = AwsS3(credentials).generate_temporary_credentials(bucket=AWS_ENV.bucket,
+            credentials = AwsS3(credentials).generate_temporary_credentials(bucket=AmazonTestEnv.bucket,
                                                                             key=tmp_test_file_name)
             assert isinstance(credentials.session_token, str) and credentials.session_token
         elif use_temporary_credentials is True:
@@ -148,28 +136,28 @@ def _test_local_to_amazon(credentials: AmazonCredentials,
         assert config.kms_key_id == credentials.kms_key_id
         rclone = RClone(destination=config)
         assert rclone.destination_config == config
-        assert rclone.copy(tmp_test_file_path, AWS_ENV.bucket) is True
+        assert rclone.copy(tmp_test_file_path, AmazonTestEnv.bucket) is True
         s3 = AwsS3(credentials)
-        assert s3.file_exists(AWS_ENV.bucket, tmp_test_file_name) is True
-        assert s3.file_equals(AWS_ENV.bucket, tmp_test_file_name, tmp_test_file_path) is True
+        assert s3.file_exists(AmazonTestEnv.bucket, tmp_test_file_name) is True
+        assert s3.file_equals(AmazonTestEnv.bucket, tmp_test_file_name, tmp_test_file_path) is True
         if config.kms_key_id:
-            assert s3.file_kms_encrypted(AWS_ENV.bucket, tmp_test_file_name) is True
-            assert s3.file_kms_encrypted(AWS_ENV.bucket, tmp_test_file_name, config.kms_key_id) is True
-        assert s3.delete_file(AWS_ENV.bucket, tmp_test_file_name) is True
-        assert s3.file_exists(AWS_ENV.bucket, tmp_test_file_name) is False
+            assert s3.file_kms_encrypted(AmazonTestEnv.bucket, tmp_test_file_name) is True
+            assert s3.file_kms_encrypted(AmazonTestEnv.bucket, tmp_test_file_name, config.kms_key_id) is True
+        assert s3.delete_file(AmazonTestEnv.bucket, tmp_test_file_name) is True
+        assert s3.file_exists(AmazonTestEnv.bucket, tmp_test_file_name) is False
 
 
 def test_google_to_local():
-    credentials = GCS_ENV.credentials()
+    credentials = GoogleTestEnv.credentials()
     config = RCloneConfigGoogle(credentials)
-    with GCS_ENV.temporary_test_file() as (tmp_test_file_path, tmp_test_file_name):
+    with temporary_test_file() as (tmp_test_file_path, tmp_test_file_name):
         rclone = RClone(destination=config)
         # TODO
-        command = rclone.copy(tmp_test_file_path, GCS_ENV.bucket, dryrun=True)
-        rclone.copy(tmp_test_file_path, GCS_ENV.bucket)
+        command = rclone.copy(tmp_test_file_path, GoogleTestEnv.bucket, dryrun=True)
+        rclone.copy(tmp_test_file_path, GoogleTestEnv.bucket)
         with temporary_file() as tmp_downloaded_file_path:
             gcs = Gcs(credentials)
-            gcs.download_file(GCS_ENV.bucket, tmp_test_file_name, tmp_downloaded_file_path)
+            gcs.download_file(GoogleTestEnv.bucket, tmp_test_file_name, tmp_downloaded_file_path)
             assert are_files_equal(tmp_test_file_path, tmp_downloaded_file_path) is True
         # TODO
         assert command is not None
