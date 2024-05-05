3a26064af6b780d250ff817ee08dcbb6de3b1d19
David Michaels <david_michaels@hms.harvard.edu>
2024-05-04 23:33:40 -0400

    rclone updates

--- submitr/scripts/submit_metadata_bundle.py
+++ submitr/scripts/submit_metadata_bundle.py
@@ -281,15 +281,13 @@ def main(simulated_args_for_testing=None):
             ping_okay = True
         if rclone_google_ping_okay is not None:
             PRINT(f"Google Cloud Storage (GCP) project: {rclone_config_google.project}")
-            if args.rclone_google_source:
-                PRINT(f"Google Cloud Storage source: {args.rclone_google_source}")
         if instance := RCloneConfigGoogle.is_google_compute_engine():
             PRINT(f"Google Compute Engine instance: {instance}")
         if args.rclone_google_source:
             if RClone().bucket_exists(args.rclone_google_source, rclone_config_google):
-                PRINT(f"Google Cloud Storage bucket: {args.rclone_google_source} ✓")
+                PRINT(f"Google Cloud Storage source: {args.rclone_google_source} ✓")
             else:
-                PRINT(f"Google Cloud Storage bucket: {args.rclone_google_source} ✗")
+                PRINT(f"Google Cloud Storage source: {args.rclone_google_source} ✗")
         if rclone_google_ping_okay is True:
             PRINT(f"Google Cloud Storage connectivity appears to be OK ✓")
         elif rclone_google_ping_okay is False:
