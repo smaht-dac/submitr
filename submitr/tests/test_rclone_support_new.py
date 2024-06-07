10f3d5db66f105c3b766ebd8d5a55b0db83cfdd2
David Michaels <david_michaels@hms.harvard.edu>
2024-06-07 11:38:09 -0400

    tests

---
+++ submitr/tests/test_rclone_support_new.py
@@ -0,0 +1,108 @@
+from contextlib import contextmanager
+import os
+import pytest
+from typing import Optional
+from dcicutils.file_utils import compute_file_md5 as get_file_checksum, get_file_size
+from dcicutils.misc_utils import create_uuid
+from dcicutils.tmpfile_utils import (
+    temporary_directory, temporary_random_file)
+from submitr.rclone.amazon_credentials import AmazonCredentials
+from submitr.rclone.rcloner import RCloner
+from submitr.rclone.rclone_amazon import RCloneAmazon
+from submitr.rclone.rclone_utils import cloud_path
+from submitr.rclone.testing.rclone_utils_for_testing_amazon import AwsS3
+
+
+# This integration test actually talks to AWS S3 and Google Cloud Storage (GCS);
+# both directly (via Python boto3 and google.cloud.storage) and via rclone.
+# The access credentials are defined by the variables as described below.
+# See testing_rclone_config for configuration parameters and comments.
+
+from submitr.tests.testing_rclone_config import (  # noqa
+
+    AMAZON_CREDENTIALS_FILE_PATH,
+    AMAZON_TEST_BUCKET_NAME,
+    AMAZON_REGION,
+    AMAZON_KMS_KEY_ID,
+
+    GOOGLE_SERVICE_ACCOUNT_FILE_PATH,
+    GOOGLE_TEST_BUCKET_NAME,
+    GOOGLE_LOCATION,
+
+    TEST_FILE_PREFIX,
+    TEST_FILE_SUFFIX,
+    TEST_FILE_SIZE,
+
+    rclone_config_setup_module,
+    rclone_config_teardown_module
+)
+
+
+# This marks this entire module as "integrtation" tests.
+# To run only integration tests:    pytest -m integration
+# To run all but integration tests: pytest -m "not integration"
+pytestmark = [pytest.mark.integration]
+
+
+def setup_module():
+    rclone_config_setup_module()
+
+
+def teardown_module():
+    rclone_config_teardown_module()
+
+
+class Amazon:
+
+    @staticmethod
+    def credentials(nokms: bool = False) -> AmazonCredentials:
+        return AmazonCredentials(AMAZON_CREDENTIALS_FILE_PATH, kms_key_id=None if nokms is True else AMAZON_KMS_KEY_ID)
+
+    @staticmethod
+    @contextmanager
+    def temporary_cloud_file(nosubfolder: bool = False, nokms: bool = False, size: Optional[int] = None) -> str:
+
+        global AMAZON_TEST_BUCKET_NAME, TEST_FILE_PREFIX, TEST_FILE_SUFFIX, TEST_FILE_SIZE
+
+        assert nosubfolder in (True, False)
+        assert nokms in (True, False)
+        assert isinstance(bucket := AMAZON_TEST_BUCKET_NAME, str) and bucket
+        if size is None: size = TEST_FILE_SIZE  # noqa
+        assert isinstance(size, int) and (size >= 0)
+
+        key = f"{TEST_FILE_PREFIX}{create_uuid()}{TEST_FILE_SUFFIX}"
+        if nosubfolder is False:
+            subfolder = f"{TEST_FILE_PREFIX}{create_uuid()}"
+            key = cloud_path.join(subfolder, key)
+
+        s3 = AwsS3(Amazon.credentials(nokms=nokms))
+        try:
+            with temporary_random_file(prefix=TEST_FILE_PREFIX, suffix=TEST_FILE_SUFFIX, nbytes=size) as tmp_file_path:
+                assert s3.upload_file(tmp_file_path, bucket, key) is True
+                assert s3.file_exists(bucket, key) is True
+                assert s3.file_size(bucket, key) == size
+                if nokms is False:
+                    assert s3.file_kms_encrypted(bucket, key, AMAZON_KMS_KEY_ID) is True
+                yield cloud_path.join(bucket, key)
+        except Exception:
+            pytest.fail("Cannot create (non-rclone) AWS S3 object!")
+            return None
+        finally:
+            s3.delete_file(bucket, key)
+
+
+def test_new() -> None:
+    global AMAZON_CREDENTIALS_FILE_PATH, AMAZON_KMS_KEY_ID
+    nokms = True
+    with Amazon.temporary_cloud_file(nokms=nokms) as amazon_cloud_path:
+        with temporary_directory() as tmpdir:
+            amazon_credentials = AmazonCredentials(AMAZON_CREDENTIALS_FILE_PATH,
+                                                   kms_key_id=None if nokms is True else AMAZON_KMS_KEY_ID)
+            amazon = RCloneAmazon(amazon_credentials)
+            RCloner(source=amazon).copy(amazon_cloud_path, tmpdir)
+            local_file_path = os.path.join(tmpdir, cloud_path.basename(amazon_cloud_path))
+            assert os.path.isfile(local_file_path)
+            assert get_file_size(local_file_path) == amazon.file_size(amazon_cloud_path)
+            assert get_file_checksum(local_file_path) == amazon.file_checksum(amazon_cloud_path)
+        pass
+    pass
--- submitr/tests/testing_rclone_config.py
+++ submitr/tests/testing_rclone_config.py
@@ -6,6 +6,8 @@ import pytest
 import tempfile
 from dcicutils.file_utils import normalize_path
 from dcicutils.tmpfile_utils import create_temporary_file_name, remove_temporary_directory, remove_temporary_file
+from submitr.rclone.amazon_credentials import AmazonCredentials
+from submitr.rclone.google_credentials import GoogleCredentials
 from submitr.rclone.rclone_google import RCloneGoogle
 from submitr.rclone.rclone_installation import RCloneInstallation
 
@@ -24,19 +26,22 @@ from submitr.rclone.rclone_installation import RCloneInstallation
 # - AWS_SECRET_ACCESS_KEY
 # - GOOGLE_CLOUD_SERVICE_ACCOUNT_JSON
 #
-# If NOT running from within GitHub actions (i.e. locally) these variables assumed to be setup,
-# and from these we obtain the above.
+# If NOT running from within GitHub actions (i.e. locally) these variables assumed
+# to be setup here/below, and from these we obtain the above.
 #
 # - AMAZON_CREDENTIALS_FILE_PATH
-#   Full path to your AWS credentials file (e.g. ~/.aws_test.smaht-wolf/credentials).
+#   Path to AWS credentials file; e.g. ~/.aws_test.smaht-test/credentials.
 # - GOOGLE_SERVICE_ACCOUNT_FILE_PATH
-#   Full path to GCP credential "service account file" exported from Google account.
+#   Path to GCP service account file exported from Google account; e.g. ~/.config/google/smaht-dac.json.
 
-# Set from testing_rclone_helpers.setup_module() IFF running from within GitHub actions.
+# Amazon/Google credentials files are setup in rclone_config_setup_module if-and-only-if
+# we are running from within GitHub actions; otherwise these are the values; this
+# the rclone_config_setup_module function should be called from the setup_module
+# function of any test module doing rclone related integration testing.
 AMAZON_CREDENTIALS_FILE_PATH = "~/.aws_test.smaht-test/credentials"
-GOOGLE_SERVICE_ACCOUNT_FILE_PATH = "~/.config/google-cloud/smaht-dac-617e0480d8e2.json"
+GOOGLE_SERVICE_ACCOUNT_FILE_PATH = "~/.config/google-cloud/smaht-dac.json"
 
-# Credentials related values less likely to need updating and hard-coded here.
+# Credential related values less likely to need updating and hard-coded here.
 AMAZON_TEST_BUCKET_NAME = "smaht-unit-testing-files"
 AMAZON_REGION = "us-east-1"
 AMAZON_KMS_KEY_ID = "27d040a3-ead1-4f5a-94ce-0fa6e7f84a95"  # fyi: not a secret
@@ -48,11 +53,10 @@ TEST_FILE_PREFIX = "test-"
 TEST_FILE_SUFFIX = ".txt"
 TEST_FILE_SIZE = 2048
 
-# Set from rclone_config_setup_module() which should run first via pytest setup_module.
+# Set from the rclone_config_setup_module function below, which should be called from
+# the setup_module function of any test module doing rclone related integration testing.
 RUNNING_FROM_WITHIN_GITHUB_ACTIONS = None
 RUNNING_ON_GOOGLE_COMPUTE_INSTANCE = None
-AMAZON_CREDENTIALS_ERROR = None
-GOOGLE_CREDENTIALS_ERROR = None
 TMPDIR = None
 
 
@@ -64,38 +68,34 @@ def is_github_actions_context():
 def rclone_config_setup_module():
 
     global TMPDIR
-    TMPDIR = tempfile.mkdtemp()
-
     global RUNNING_FROM_WITHIN_GITHUB_ACTIONS
     global RUNNING_ON_GOOGLE_COMPUTE_INSTANCE
-
-    global AMAZON_CREDENTIALS_ERROR
     global AMAZON_CREDENTIALS_FILE_PATH
-
-    global GOOGLE_CREDENTIALS_ERROR
     global GOOGLE_SERVICE_ACCOUNT_FILE_PATH
 
+    TMPDIR = tempfile.mkdtemp()
     RUNNING_FROM_WITHIN_GITHUB_ACTIONS = is_github_actions_context()
     RUNNING_ON_GOOGLE_COMPUTE_INSTANCE = RCloneGoogle.is_google_compute_engine()
 
-    def print_integation_testing_context():
-        print(f"Running from within GitHub Actions:"
-              f" {'YES' if RUNNING_FROM_WITHIN_GITHUB_ACTIONS else 'NO'}")
-        print(f"Running on a Google Compute Engine (GCE) instance:"
-              f" {'YES' if RUNNING_ON_GOOGLE_COMPUTE_INSTANCE else 'NO'}")
+    print(f"Running from within GitHub Actions:"
+          f" {'YES' if RUNNING_FROM_WITHIN_GITHUB_ACTIONS else 'NO'}")
+    print(f"Running on a Google Compute Engine (GCE) instance:"
+          f" {'YES' if RUNNING_ON_GOOGLE_COMPUTE_INSTANCE else 'NO'}")
 
-    print_integation_testing_context()
+    # Make sure rclone is installed.
 
     assert RCloneInstallation.install() is not None
     assert RCloneInstallation.is_installed() is True
+
+    # Obtain Amazon credentials for testing.
 
     if RUNNING_FROM_WITHIN_GITHUB_ACTIONS:
         access_key_id = os.environ.get("AWS_ACCESS_KEY_ID", None)
         secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
         session_token = os.environ.get("AWS_SESSION_TOKEN", None)
         if not (access_key_id and secret_access_key):
-            AMAZON_CREDENTIALS_ERROR = "AWS acesss keys not defined!"
-            pytest.fail("Integration test setup error: AWS acesss keys not defined!")
+            pytest.fail(f"Test setup ERROR: AWS acesss keys not defined!"
+                        f" GitHub secrets should be defined for AWS credentials.")
         else:
             AMAZON_CREDENTIALS_FILE_PATH = create_temporary_file_name()
             with open(AMAZON_CREDENTIALS_FILE_PATH, "w") as f:
@@ -105,17 +105,26 @@ def rclone_config_setup_module():
                 f.write(f"aws_session_token={session_token}\n") if session_token else None
             os.chmod(AMAZON_CREDENTIALS_FILE_PATH, 0o600)  # for security
             print(f"Amazon Credentials:")
-            print(f"- AWS_ACCESS_KEY_ID: {access_key_id[:2]}{(len(access_key_id) - 2) * '*'}")
-            print(f"- AWS_SECRET_ACCESS_KEY: {len(secret_access_key) * '*'}")
-            print(f"- AMAZON_CREDENTIALS_FILE_PATH: {AMAZON_CREDENTIALS_FILE_PATH}")
+            print(f"- AWS access key ID: {access_key_id[:2]}******")
+            print(f"- AWS secret access key: ********")
+            if session_token:
+                print(f"- AWS session token: {session_token[:2]}******")
+            print(f"- AWS credentials file: {AMAZON_CREDENTIALS_FILE_PATH}")
     else:
         if not (AMAZON_CREDENTIALS_FILE_PATH and
                 os.path.isfile(normalize_path(AMAZON_CREDENTIALS_FILE_PATH, expand_home=True))):
-            AMAZON_CREDENTIALS_ERROR = "No Amazon credentials file defined!"
+            pytest.fail(f"Test setup ERROR: No Amazon credentials file defined!"
+                        f" The testing_clone_config.AMAZON_CREDENTIALS_FILE_PATH variable"
+                        f" should be set to the path of an AWS credentials file.")
+
+    # Obtain Google credentials for testing.
 
     if RUNNING_FROM_WITHIN_GITHUB_ACTIONS:
         if not (service_account_json_string := os.environ.get("GOOGLE_CLOUD_SERVICE_ACCOUNT_JSON")):
-            pytest.fail("Integration test setup error: No Google credentials defined!")
+            pytest.fail("Test setup ERROR:"
+                        f"No Google credentials defined!"
+                        f" GitHub secret GOOGLE_CLOUD_SERVICE_ACCOUNT_JSON"
+                        f" should be set to the path of a GCS service account JSON file.")
         service_account_json = json.loads(service_account_json_string)
         google_service_account_file_path = create_temporary_file_name(suffix=".json")
         with open(google_service_account_file_path, "w") as f:
@@ -129,20 +138,32 @@ def rclone_config_setup_module():
         if not (GOOGLE_SERVICE_ACCOUNT_FILE_PATH and
                 os.path.isfile(normalize_path(GOOGLE_SERVICE_ACCOUNT_FILE_PATH, expand_home=True))):
             if not RCloneGoogle.is_google_compute_engine():
-                GOOGLE_CREDENTIALS_ERROR = "No Google credentials file defined!"
+                pytest.fail(f"Test setup ERROR: No Google credentials file defined!"
+                            f" The testing_rclone_config.GOOGLE_SERVICE_ACCOUNT_FILE_PATH variable"
+                            f" should be set to the path of a GCS service account JSON file.")
             else:
                 # Google credentials can be None on a GCE instance; i.e. no service account file needed.
                 GOOGLE_SERVICE_ACCOUNT_FILE_PATH = None
 
+    # Actually test the Amazon credentials.
+    amazon_credentials = AmazonCredentials(AMAZON_CREDENTIALS_FILE_PATH)
+    if not amazon_credentials.ping():
+        pytest.fail("Test setup ERROR: Amazon credentials do not appear to work!")
+
+    # Actually test the Google credentials.
+    google_credentials = GoogleCredentials(GOOGLE_SERVICE_ACCOUNT_FILE_PATH)
+    if not google_credentials.ping():
+        pytest.fail("Test setup ERROR: Google credentials do not appear to work!")
+
     # Just make sure no interference from credentials not explicitly setup for testing.
+
     os.environ.pop("AWS_DEFAULT_REGION", None)
     os.environ.pop("AWS_ACCESS_KEY_ID", None)
     os.environ.pop("AWS_SECRET_ACCESS_KEY", None)
     os.environ.pop("AWS_SESSION_TOKEN", None)
-    assert os.environ.get("AWS_DEFAULT_REGION", None) is None
-    assert os.environ.get("AWS_ACCESS_KEY_ID", None) is None
-    assert os.environ.get("AWS_SECRET_ACCESS_KEY", None) is None
-    assert os.environ.get("AWS_SESSION_TOKEN", None) is None
+    os.environ.pop("AWS_SHARED_CREDENTIALS_FILE", None)
+    os.environ.pop("AWS_CONFIG_FILE", None)
+    os.environ.pop("GOOGLE_APPLICATION_CREDENTIALS", None)
 
 
 def rclone_config_teardown_module():
