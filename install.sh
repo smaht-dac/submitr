bdfb1d24fcf366b0d7acd9eaaf994b930e5afcad
David Michaels <david_michaels@hms.harvard.edu>
2024-06-14 17:16:37 -0400

    binaries

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BASEURL=https://github.com/smaht-dac/submitr/releases/download/latest/submitr-
+TARGET=submitr
+
+UNAME=`uname`
+ARCH=`arch`
+if [ $UNAME == 'Darwin' ] ; then
+        URL=$BASEURL-macos
+else
+    if [ $ARCH == 'arm64' -o $ARCH == 'aarch64' ] ; then
+        URL=$BASEURL-linux-arm
+    else
+        URL=$BASEURL-linux-x86
+    fi
+fi
+
+curl -L -o $TARGET $URL -s
+chmod a+x $TARGET
+ls -l $TARGET
