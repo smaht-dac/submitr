fa7324012beded95ff9d6def1d1de39dacf45288
David Michaels <david_michaels@hms.harvard.edu>
2024-06-17 16:35:56 -0400

    Binary build development

--- install.sh
+++ install.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
+# Download the most recent release of the smaht-submitr binary for the calling platform.
+
 LATEST_RELEASE_INFO_URL=https://api.github.com/repos/smaht-dac/submitr/releases/latest
 TARGET=submitr
 
-UNAME=`uname`
-ARCH=`arch`
-if [ $UNAME == 'Darwin' ] ; then
-    if [ $ARCH == 'arm64' -o $ARCH == 'aarch64' ] ; then
-        FILE=submitr-macos-arm
+function download_url() {
+    UNAME=`uname`
+    ARCH=`arch`
+    if [ $UNAME == 'Darwin' ] ; then
+        if [ $ARCH == 'arm64' -o $ARCH == 'aarch64' ] ; then
+            FILE=submitr-macos-arm
+        else
+            FILE=submitr-macos-x86
+        fi
     else
-        FILE=submitr-macos-x86
+        if [ $ARCH == 'arm64' -o $ARCH == 'aarch64' ] ; then
+            FILE=submitr-linux-arm
+        else
+            FILE=submitr-linux-x86
+        fi
     fi
-else
-    if [ $ARCH == 'arm64' -o $ARCH == 'aarch64' ] ; then
-        FILE=submitr-linux-arm
-    else
-        FILE=submitr-linux-x86
+    DOWNLOAD_URL=`curl -L -s $LATEST_RELEASE_INFO_URL | sed -nE "s/.*\"browser_download_url\": \"(https:\/\/[^\"]*$FILE)\".*/\1/p"`
+}
+
+download_url
+if [ -z $DOWNLOAD_URL ] ; then
+    # Retry once or twice; in testing within GitHub Actions only, intermittently fails first time.
+    echo "Retrying once ..."
+    download_url
+    if [ -z $DOWNLOAD_URL ] ; then
+        echo "Retrying twice ..."
+        download_url
     fi
 fi
 
-DOWNLOAD_URL=`curl -L -s $LATEST_RELEASE_INFO_URL | sed -nE "s/.*\"browser_download_url\": \"(https:\/\/[^\"]*$FILE)\".*/\1/p"`
 echo "Downloading $DOWNLOAD_URL to $TARGET"
 curl -L -s -o $TARGET $DOWNLOAD_URL
 chmod a+x $TARGET
