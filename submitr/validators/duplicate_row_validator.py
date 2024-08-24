4369404375ebd8fb96c33d68c89182487eed3c93
David Michaels <david_michaels@hms.harvard.edu>
2024-08-23 23:43:16 -0400

    some validators refactoring

--- submitr/validators/duplicate_row_validator.py
+++ submitr/validators/duplicate_row_validator.py
@@ -1,3 +1,4 @@
+import json
 from submitr.validators.utils.structured_data_validator_hook import structured_data_validator_sheet_hook
 from dcicutils.structured_data import StructuredDataSet
 
@@ -14,4 +15,18 @@ _DUPLICATE_ROW_DETECTIONS_SHEETS = [
 
 @structured_data_validator_sheet_hook(_DUPLICATE_ROW_DETECTIONS_SHEETS)
 def duplicate_row_validator(structured_data: StructuredDataSet, sheet_name: str, data: dict) -> None:
-    print(f"\nTODO: DUPLICATE ROW VALIDATOR: [{sheet_name}]")
+    if _has_duplicate_elements(data):
+        structured_data.note_validation_error(f"Duplicate rows in sheet: {sheet_name}", sheet_name, 0)
+        print(f"\nTODO: DUPLICATE ROW VALIDATOR: [{sheet_name}] -> DUPLICATES!")
+    else:
+        print(f"\nTODO: DUPLICATE ROW VALIDATOR: [{sheet_name}] -> OK")
+
+
+def _has_duplicate_elements(array: list[dict]) -> bool:
+    seen = set()
+    for element in array:
+        serialized_element = json.dumps(element, sort_keys=True)
+        if serialized_element in seen:
+            return True
+        seen.add(serialized_element)
+    return False
