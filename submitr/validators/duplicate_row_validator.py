20848ddf9fcaf57af46c041b4aa98cad14071460
David Michaels <david_michaels@hms.harvard.edu>
2024-08-23 23:30:24 -0400

    some validators refactoring

--- submitr/validators/duplicate_row_validator.py
+++ submitr/validators/duplicate_row_validator.py
@@ -1,7 +1,17 @@
 from submitr.validators.utils.structured_data_validator_hook import structured_data_validator_sheet_hook
 from dcicutils.structured_data import StructuredDataSet
 
+_DUPLICATE_ROW_DETECTIONS_SHEETS = [
+    "AnalytePreparation",
+    "Basecalling",
+    "LibraryPreparation",
+    "PreparationKit",
+    "Sequencing",
+    "Software",
+    "Treatment"
+]
 
-@structured_data_validator_sheet_hook(["CellLine", "Analyte", "FileSet"])
+
+@structured_data_validator_sheet_hook(_DUPLICATE_ROW_DETECTIONS_SHEETS)
 def duplicate_row_validator(structured_data: StructuredDataSet, sheet_name: str, data: dict) -> None:
-    print(f'\nxxxxx/validate[{sheet_name}')
+    print(f"\nTODO: DUPLICATE ROW VALIDATOR: [{sheet_name}]")
