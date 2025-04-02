720cd3014b2909b8ad5abcec6b775322c3d59911
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2025-04-02 16:57:58 -0400

    Fix strand validator

--- submitr/validators/strand_required.py
+++ submitr/validators/strand_required.py
@@ -1,13 +1,10 @@
 from dcicutils.structured_data import StructuredDataSet
 from submitr.validators.decorators import structured_data_validator_finish_hook
 
-
-# This "validator" does not use any of the decorator hooks defined in this directory;
-# it is a simple function (require_strand) called from submission.py.
-# It reports if and LibraryPrepration items defined in the spreadsheet (StructuredDataSet) are missing the strand property if they are linked to an RNA library
+# Validator that reports if any Library items defined in the spreadsheet (StructuredDataSet) are missing the strand property in LibraryPreparation if they are linked to RNA Analyte items
 _LIBRARY_SCHEMA_NAME = "Library"
 _ANALYTE_PROPERTY_NAME = 'analytes'
-_LIBRARY_PREPARATION_PROPERTY_NAME = 'library_preparation'
+_LIBRARY_PREP_PROPERTY_NAME = 'library_preparation'
 _ANALTYE_SCHEMA_NAME = "Analyte"
 _MOLECULE_PROPERTY_NAME = "molecule"
 _RNA_VALUE_NAME = "RNA"
@@ -21,18 +18,29 @@ def _strand_required(structured_data: StructuredDataSet, **kwargs) -> None:
         return
     for item in data:
         if _ANALYTE_PROPERTY_NAME in item and (submitted_id := item.get("submitted_id")):
-            if isinstance(analytes := structured_data.data.get(_ANALTYE_SCHEMA_NAME), list):
+            if isinstance(analytes := [ 
+                analyte_item for analyte_item in structured_data.data.get(_ANALTYE_SCHEMA_NAME)
+                if analyte_item.get("submitted_id") in item.get(_ANALYTE_PROPERTY_NAME)
+            ], list):
                 for analyte in analytes:
                     if _RNA_VALUE_NAME in analyte.get(_MOLECULE_PROPERTY_NAME):
-                        if _LIBRARY_PREPARATION_PROPERTY_NAME in item:
-                            if isinstance(library_prep := structured_data.data.get(_LIBRARY_PREP_SCHEMA_NAME), dict):
+                        if _LIBRARY_PREP_PROPERTY_NAME in item:
+                            if (library_prep := [
+                                lp_item for lp_item in structured_data.data.get(_LIBRARY_PREP_SCHEMA_NAME)
+                                if lp_item.get("submitted_id") == item.get(_LIBRARY_PREP_PROPERTY_NAME)
+                            ][0]):
                                 if _STRAND_PROPERTY_NAME not in library_prep:
                                     structured_data.note_validation_error(
-                                        f"{_LIBRARY_SCHEMA_NAME}."
-                                        f" ({_LIBRARY_PREP_SCHEMA_NAME} Property {_STRAND_PROPERTY_NAME} is required for RNA libraries: {submitted_id}"
+                                        f"{_LIBRARY_SCHEMA_NAME}:"
+                                        f" {_LIBRARY_PREP_SCHEMA_NAME} property {_STRAND_PROPERTY_NAME} is required for RNA libraries: {submitted_id}"
                                     )
+                                    # structured_data.note_validation_error(
+                                    #     f"{_LIBRARY_SCHEMA_NAME}: "
+                                    #     f"{_LIBRARY_PREP_SCHEMA_NAME} item {library_prep.get("submitted_id")}"
+                                    #     f" is missing property {_STRAND_PROPERTY_NAME}, which is required for RNA libraries: {submitted_id}"
+                                    # )
                         else:
                             structured_data.note_validation_error(
-                                f"{_LIBRARY_SCHEMA_NAME}."
-                                f" ({_LIBRARY_PREP_SCHEMA_NAME} Property {_STRAND_PROPERTY_NAME} is required for RNA libraries: {submitted_id}"
+                                f"{_LIBRARY_SCHEMA_NAME}:"
+                                f" {_LIBRARY_PREP_SCHEMA_NAME} property {_STRAND_PROPERTY_NAME} is required for RNA libraries: {submitted_id}"
                             )
