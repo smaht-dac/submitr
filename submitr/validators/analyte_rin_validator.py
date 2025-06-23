b106e294d9b511d1902849d939a26b49d7bb2f14
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2025-06-23 16:08:37 -0400

    Add rin required validator

---
+++ submitr/validators/analyte_rin_validator.py
@@ -0,0 +1,29 @@
+from dcicutils.structured_data import StructuredDataSet
+from submitr.validators.decorators import structured_data_validator_finish_hook
+
+import collections
+
+# Validator that reports if any Analyte items that have molecule RNA
+# do not have the `rna_integrity_number` property filled out
+_ANALYTE_SCHEMA_NAME = "Analyte"
+_RIN_PROPERTY_NAME = "rna_integrity_number"
+_MOLECULE_PROPERTY_NAME = "molecule"
+_RNA_VALUE = "RNA"
+
+
+@structured_data_validator_finish_hook
+def _analyte_rin_validator(structured_data: StructuredDataSet, **kwargs) -> None:
+    if not isinstance(data := structured_data.data.get(_ANALYTE_SCHEMA_NAME), list):
+        return
+    for item in data:
+        if _MOLECULE_PROPERTY_NAME in item and (
+            submitted_id := item.get("submitted_id")
+        ):
+            if _RNA_VALUE in item.get(_MOLECULE_PROPERTY_NAME) and _RIN_PROPERTY_NAME not in item:
+                # rna_integrity_number not present
+                structured_data.note_validation_error(
+                    f"{_ANALYTE_SCHEMA_NAME}:"
+                    f" item {submitted_id}"
+                    f" property {_RIN_PROPERTY_NAME} is required"
+                    f" for {_RNA_VALUE} analytes."
+                )
