4888f6e2a7a23fe2a7ac55fabd953c9030e8caab
aschroed <andrew_schroeder@hms.harvard.edu>
2026-02-12 11:56:14 -0500

    add a new tissue validator for consistent preservation type and code

--- submitr/validators/tissue_external_id_validator.py
+++ submitr/validators/tissue_external_id_validator.py
@@ -1,5 +1,8 @@
+from typing import Dict
+from dcicutils import ff_utils
 from dcicutils.structured_data import StructuredDataSet
 from submitr.validators.decorators import structured_data_validator_finish_hook
+from utils.dcicutils import structured_data
 
 
 # Validator that reports if any Tissue items are linked to Donor items
@@ -11,6 +14,8 @@ _DONOR_PROPERTY_NAME = "donor"
 _EXTERNAL_ID_PROPERTY_NAME = "external_id"
 _DONOR_SCHEMA_NAME = "Donor"
 _NDRI_SUBMISSION_CENTER = "NDRI"
+_TISSUE_TERM_PROPERTY_NAME = "uberon_id"
+_OT_TYPE = "OntologyTerm"
 
 
 @structured_data_validator_finish_hook
@@ -37,4 +42,44 @@ def _tissue_external_id_validator(structured_data: StructuredDataSet, **kwargs) 
                             f" item {submitted_id}"
                             f" external_id {tissue_external_id} does not"
                             f" match Donor external_id {donor_external_id}."
+                        )
+
+
+def _get_term_info(term_id: str, key: Dict) -> str:
+    term_info = {}
+    #import pdb; pdb.set_trace()
+    query = f"/search/?type={_OT_TYPE}&identifier={term_id}"
+    result = ff_utils.search_metadata(query, key)
+    if result and len(result) == 1:
+        term = result[0]
+        if term and (pids := term.get('valid_protocol_ids')):
+            for pid in pids:
+                code, ptype = (pid.split('_', 1) + [''])[:2]
+                term_info[code] = ptype
+    return term_info
+
+
+@structured_data_validator_finish_hook
+def _tissue_preservation_type_validator(structured_data: StructuredDataSet, **kwargs) -> None:
+    if not isinstance(data := structured_data.data.get(_TISSUE_SCHEMA_NAME), list):
+        return
+    seen = {}
+    for item in data:
+        if _TISSUE_TERM_PROPERTY_NAME in item and (
+             term_id := item.get(_TISSUE_TERM_PROPERTY_NAME)
+        ):
+            if not (term_info := seen.get(term_id)):
+                term_info = _get_term_info(term_id, structured_data.portal.key)
+                seen[term_id] = term_info
+
+        if ptype := item.get("preservation_type"):
+            for code, expected_ptype in term_info.items():
+                if expected_ptype and code in item.get(_EXTERNAL_ID_PROPERTY_NAME, ""):
+                    if expected_ptype not in ptype:
+                        structured_data.note_validation_error(
+                            f"{_TISSUE_SCHEMA_NAME}:"
+                            f" item {item.get('submitted_id')}"
+                            f" preservation_type {ptype} does not match"
+                            f" expected preservation type {expected_ptype}"
+                            f" for code {code} in Uberon term {term_id}."
                         )
